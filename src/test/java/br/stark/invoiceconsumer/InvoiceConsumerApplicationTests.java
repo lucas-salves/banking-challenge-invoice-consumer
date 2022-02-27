3f2b359598ec18ee86b8bb621bddf15b0914132f
lucas-salves <salves.lucas@outlook.com>
2022-02-27 15:32:26 -0300

    initial commit

---
+++ src/test/java/br/stark/invoiceconsumer/InvoiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.stark.invoiceconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
