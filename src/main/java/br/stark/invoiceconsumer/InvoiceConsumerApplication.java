0fe725e7702c761712dbb4a044f06bb27394b11a
lucas-salves <salves.lucas@outlook.com>
2022-02-27 18:43:47 -0300

    Feat InvoiceConsumer
    InvoiceConsumer receives the "invoiced" messages from the webhook event and sends it to the "credited_invoices" consumer for the trandfer of values to be carried out.

--- src/main/java/br/stark/invoiceconsumer/InvoiceConsumerApplication.java
+++ src/main/java/br/stark/invoiceconsumer/InvoiceConsumerApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvoiceConsumerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(InvoiceConsumerApplication.class, args);
+                
+                InvoiceWorker.main(args);
 	}
 
 }
